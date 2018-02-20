0e73d0d671db7505d8f11d677d9683e300c91cb0
ahmed3010 <33694547+ahmed3010@users.noreply.github.com>
2018-02-20 21:19:18 +0200

    Initial commit

---
+++ app/src/main/java/com/shohayeb/businesscard/MainActivity.java
@@ -0,0 +1,33 @@
+package com.shohayeb.businesscard;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void website(View view) {
+        Uri webpage = Uri.parse(getString(R.string.website_value));
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void call(View view) {
+        String phoneNumber = getString(R.string.phone_value);
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+}
